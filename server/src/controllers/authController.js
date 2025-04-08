a94b54f965684fde328a58359c63d489c7f692f7
Pradip Gharge <59740723+pradipgharge@users.noreply.github.com>
2025-04-09 01:05:31 +0530

    feat: user registration & password hashing

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -4,10 +4,64 @@ const bcrypt = require("bcryptjs");
 
 // Register user controller
 const registerUser = async (req, res) => {
-  res.status(501).json({
-    status: "error",
-    message: "This route is not implemented yet",
-  });
+  try {
+    const { username, email, password } = req.body;
+
+    // Validate required fields
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Please provide all required fields",
+      });
+    }
+
+    // Check if user already exists (By Email or Username)
+    const userExists = await User.findOne({
+      // or operator checks for both email & username
+      $or: [{ email }, { username }],
+    });
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ status: "fail", message: "User already exists" });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create user
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    // Generate token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    // Send Success Response
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: {
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+        },
+        token,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: "An Error occured during registration",
+      error: error.message,
+    });
+  }
 };
 
 module.exports = { registerUser };
